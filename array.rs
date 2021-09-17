e077bc3fc6761163fc1c467154abdb81754b276d
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2021-09-16 20:48:18 -0700

    name change

--- array.rs
+++ array.rs
@@ -1,13 +1,13 @@
 
 fn main() {
     //let cum = String::new();
-    let mut cum: String = String::new();
+    let mut cumulative: String = String::new();
     let a: [i32; 5] = [1,2,3,4,5];
 
     for num in a.iter() {
-        cum += &num.to_string();
-        cum += " ";
+        cumulative += &num.to_string();
+        cumulative += " ";
     }
 
-    println!("{}", cum);
+    println!("{}", cumulative);
 }
